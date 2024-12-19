5e5818d68058a7aee61dcf50308abf2d1b931d9e
Noureldeen-Elbialy <nourelbi292@gmail.com>
2024-12-19 03:37:52 +0200

    start peoject

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className="fixed inset-0 w-screen h-screen bg-slate-900 flex justify-center items-center">
+        <div className="container text-center">
+          <h1 className='text-5xl text-blue-800 font-bold'>start new <span className='text-red-700'>Project!</span></h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
